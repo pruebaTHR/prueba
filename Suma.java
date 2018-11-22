7953bc8a8c01bb1a66174e94070a1b7bc0191f12
Edgar García Leyva <edgargarcialeyva@MacBook-Pro-de-Edgar.local>
2018-11-22 06:55:08 -0600

    Se ha agregado una clase de Java para sumar 2 numeros

---
+++ Suma.java
@@ -0,0 +1,20 @@
+package suma;
+
+/**
+ *
+ * @author edgargarcialeyva
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero1 = 50;
+        int numero2 = 100;
+        int resultado = (numero1 + numero2);
+        
+        System.out.println("La suma de numero1 y numero2 es: " + resultado);
+    }
+    
+}
